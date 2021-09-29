9b56d74ca1deb22d13f26418d23589e16daedb9f
Napolitain <mxboucher@gmail.com>
2021-09-29 11:58:38 -0700

    init + triangle strip

---
+++ fshader.glsl
@@ -0,0 +1,14 @@
+#version 140
+
+uniform sampler2D texture;
+
+in vec2 v_texcoord;
+in vec3 v_position;
+//! [0]
+void main()
+{
+    // Set fragment color from texture
+    gl_FragColor = texture2D(texture, v_texcoord);
+}
+//! [0]
+
